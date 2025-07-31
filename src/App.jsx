06fa450287069370aa80c01a7f54b776c62cf875
Misasoa Robison <robisonmisasoa@gmail.com>
2025-07-31 14:59:19 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
